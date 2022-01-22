e8566e30b034de1a0522223f7d9e275939621976
bchanowski <bartosz.chanowski@gmail.com>
2022-01-22 14:46:58 +0100

    Lab8 drugi commit

--- lab8/lab8/src/App.js
+++ lab8/lab8/src/App.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react'
 import useCollapse from 'react-collapsed'
 import './App.css';
+import { Button, Card } from 'react-bootstrap';
+
+function MoreText() {
+    const [isExpanded, setExpanded] = useState(false)
+    const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
+
+    return(
+        <div>
+            <p {...getCollapseProps()}>Więcej tekstu</p>
+            <Button style={{  marginTop: '10px' }}
+                {...getToggleProps({
+                    onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+                })}
+                >
+                {isExpanded ? 'Pokaż mniej' : 'Pokaż więcej'}
+            </Button>
+        </div>
+    );
+}
 
 function App() {
   const [isExpanded, setExpanded] = useState(false)
@@ -8,14 +27,18 @@ function App() {
 
   return (
       <div>
-        <button
-            {...getToggleProps({
-              onClick: () => setExpanded((prevExpanded) => !prevExpanded),
-            })}
-        >
-          {isExpanded ? 'Collapse' : 'Expand'}
-        </button>
-        <section {...getCollapseProps()}>Collapsed content 🙈</section>
+          <Card className="text-center center" border="primary" style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto', marginTop: '18rem' }}>
+              <Card.Body>
+                <Button variant="outline-primary" style={{ marginBottom: '10px' }}
+                    {...getToggleProps({
+                        onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+                    })}
+                >
+                    {isExpanded ? 'Ukryj' : 'Pokaż'}
+                </Button>
+                  <p {...getCollapseProps()}>Testowy tekst<MoreText /></p>
+              </Card.Body>
+          </Card>
       </div>
   )
 }
